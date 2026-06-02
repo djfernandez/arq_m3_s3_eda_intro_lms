31910a96dfa1b3f2a2b4aad6f8b39265e6d09775
jgomezz <jgomezz@gmail.com>
2026-06-01 21:48:27 -0500

    add consumer

--- src/main/java/pe/edu/tecsup/lms/courses/domain/event/CourseCreatedEvent.java
+++ src/main/java/pe/edu/tecsup/lms/courses/domain/event/CourseCreatedEvent.java
@@ -2,10 +2,12 @@ package pe.edu.tecsup.lms.courses.domain.event;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 import pe.edu.tecsup.lms.shared.domain.event.DomainEvent;
 
 @AllArgsConstructor
 @Getter
+@ToString
 public class CourseCreatedEvent extends DomainEvent {
 
     private final String courseId;
--- src/main/java/pe/edu/tecsup/lms/notifications/application/event/CourseEventHandler.java
+++ src/main/java/pe/edu/tecsup/lms/notifications/application/event/CourseEventHandler.java
@@ -1,7 +1,22 @@
 package pe.edu.tecsup.lms.notifications.application.event;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import pe.edu.tecsup.lms.courses.domain.event.CourseCreatedEvent;
+
 /**
  * Es el consumidor de eventos
  */
+@Slf4j
+@Component
 public class CourseEventHandler {
+
+    @EventListener
+    public void handleCourseCreated(CourseCreatedEvent event)
+    {
+        log.info("Course created event received: {}", event);
+
+    }
+
 }
